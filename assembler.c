c7d3d8a56a1937beac0e9c45235eaa7f878d132c
Roee Bitton <BIROEE7@telem.openu.ac.il>
2024-06-13 16:40:51 +0000

    Added simple I\O.

--- assembler.c
+++ assembler.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  char name[100];
+  printf("Hello There.\n");
+  fgets(name, 100, stdin);
+  name[strlen(name)-1] = '\0';
+  printf("%s.\n",name);
   return 0;
 }
